c88f1a14411116e2f992a9e65c958a29fcaf940c
LeahPines <leajp9973@gmail.com>
2025-01-15 13:45:26 +0200

    uploading folder

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+project_id          = "terraform-leah-pines"
+region              = "me-west1"
+instance_name_prefix = "leah-pines"
+machine_type        = "e2-micro"
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region where resources will be created"
+  type        = string
+}
+
+variable "instance_name_prefix" {
+  description = "Prefix for instance and resource names"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Machine type for the instance template"
+  type        = string
+  default     = "e2-micro" # Default value, can be overridden
+}
